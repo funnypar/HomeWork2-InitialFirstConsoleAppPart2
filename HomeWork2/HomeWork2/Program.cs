0a5662a5646e5fb2b89e6eafedfafa8e45d22b2c
Mopano <p.r.7.norouzi@gmail.com>
2026-06-02 21:37:10 +0300

    Implementation Of Tasks

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,19 @@
+﻿using HomeWork2.Task3;
+using HomeWork2.Task4;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run Task 3 
+            //Statistics statistics = new Statistics();
+            //statistics.Run();
+
+            // Run Task 4 
+            ArrayAnalyzer arrayAnalyzer = new ArrayAnalyzer();  
+            arrayAnalyzer.Run();
+        }
+    }
+}
---
+++ HomeWork2/HomeWork2/Task1/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork2.Task1
+{
+    public class Student
+    {
+        // We do not need to define "age" and "name" ! The propeties create them in backgrond and manage them
+        // And we can easily write logics for them
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+    }
+}
